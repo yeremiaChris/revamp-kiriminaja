22cccf036e1b25de2d3aed21d66b35ddbb184d04
yeremiaChris <yeremia997@gmail.com>
2022-12-03 08:00:16 +0700

    integration owl-carousel

--- components/home/Carousel.vue
+++ components/home/Carousel.vue
@@ -1,42 +1,41 @@
 <template>
-  <div class="mt-16 px-20 flex gap-10 items-center">
-    <!-- image -->
-    <img
-      src="https://kiriminaja.com/storage/Slider/November22/1669004316_1668074906_slider november (1).png"
-      alt="image"
-      width="437"
-    />
+  <div>
+    <client-only placeholder="Setup the carousel...">
+      <Carousel
+        :items="1"
+        :nav="false"
+        :autoplay="true"
+        :autoplay-timeout="3000"
+      >
+        <div
+          class="mt-16 px-20 flex gap-10 items-center"
+          v-for="item in 3"
+          :key="item"
+        >
+          <!-- image -->
+          <img
+            src="https://kiriminaja.com/storage/Slider/November22/1669004316_1668074906_slider november (1).png"
+            alt="image"
+            width="437"
+            class="rounded-lg self-stretch"
+          />
 
-    <!-- content -->
-    <div class="flex flex-col gap-3 items-start">
-      <span class="px-3 py-2 text-secondary rounded-full bg-secondary-opacity"
-        >#1 Info</span
-      >
-      <h2 class="text-5xl font-bold">November Untung Bener</h2>
-      <p class="text-xl">
-        Cara cerdas KIRIM PAKET pakai KiriminAja banyak diskon dan dapatkan
-        peluang bawa pulang hadiahnya. Yuk buruan daftar dan kirim paketmu
-        sekarang!
-      </p>
-      <ul class="grid gap-4 font-medium">
-        <li v-for="item in 3" :key="item" class="flex items-center gap-2">
-          <div class="bg-secondary rounded-full p-[3px]">
-            <img class="w-4" src="/icon/check.svg" alt="check" />
-          </div>
-          Tanpa Biaya Pendaftaran
-        </li>
-      </ul>
-      <button
-        class="bg-primary rounded-full px-8 text-lg py-3 text-white font-medium"
-      >
-        Selengkapnya
-      </button>
-    </div>
+          <!-- content -->
+          <HomeCarouselContent />
+        </div>
+      </Carousel>
+    </client-only>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      options: {
+        items: 1,
+      },
+    };
+  },
+};
 </script>
-
-<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   css: ["@/assets/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: "plugins/owl.js", ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
