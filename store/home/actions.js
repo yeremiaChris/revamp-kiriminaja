f6b9cb7ff0820fa2afd7ca37a4735282a070ed39
yeremiaChris <yeremia997@gmail.com>
2022-12-04 23:26:51 +0700

    commit

--- store/home/actions.js
+++ store/home/actions.js
@@ -1,12 +1,10 @@
 export default {
   async fetchCarouselData({ commit }) {
-    commit("SET_LOADING", true);
     try {
       const { data } = await this.$axios.get(`/posts`);
       commit("GET_CAROUSEL_DATA", data.slice(0, 3));
     } catch (error) {
       throw new Error(error);
     }
-    commit("SET_LOADING", false);
   },
 };
